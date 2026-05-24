5dc958f45005420a9cc41c3c06bf3806cdaa6de2
gabsjf <gabrieljf268@gmail.com>
2026-05-23 22:37:40 -0300

    Adiciona sprites de animação e de ataque

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    public Animator playerAnimator;
+    public void PlayAnimation(string animationName)
+    {
+        playerAnimator.Play(animationName);
+    }
+
+    private void Start()
+    {
+        PlayAnimation("PlayerIdle");
+    }
+}
+ 
